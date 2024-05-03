1b64107e0db509188d2bb8c0c552c107eed5f4ef
Andrés <andresmholguin@gmail.com>
2024-05-03 02:17:07 -0500

    Actualiza rutas de imágenes

--- src/App.jsx
+++ src/App.jsx
@@ -21,13 +21,13 @@ function App() {
       <div className="flex flex-col my-5">
         <div className="flex justify-center">
           <img
-            src="src\assets\logo_tertulia.svg"
+            src="src/assets/logo_tertulia.svg"
             alt="Logo Museo La Tertulia"
             className="px-2 h-28"
           />
           <img
-            src="src\assets\univalle.png"
-            alt="Logo Museo La Tertulia"
+            src="src/assets/univalle.png"
+            alt="Logo Univalle"
             className="px-2 h-28 "
           />
         </div>
